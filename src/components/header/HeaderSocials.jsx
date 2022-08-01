a49d7685cc5f49304d8eeb2ce65fddb875e05a90
YSquid <102336493+YSquid@users.noreply.github.com>
2022-08-01 16:49:46 -0600

    More header styling
    Aligning header elements - CTA and socials classes
    
    After pseudoclass used to make the line under socials
    
    Used some flexbox tricks to send socials to left and bottom of header element

--- src/components/header/HeaderSocials.jsx
+++ src/components/header/HeaderSocials.jsx
@@ -1,12 +1,14 @@
 import React from 'react'
-import {BsLinkedin} from 'react-icons/bs'
-import {BsGithub} from 'react-icons/bs'
+import {BsLinkedin, BsGithub} from 'react-icons/bs'
+import {FaTwitter} from 'react-icons/fa'
+
 
 const HeaderSocials = () => {
   return (
     <div className="header__socials">
-        <a href='https://linkedin.com/in/ahmad-kariem/' target='_blank'><BsLinkedin /></a>
-        <a href='https://github.com/Ysquid' target='_blank'><BsGithub /></a>
+        <a href='https://linkedin.com/in/ahmad-kariem/' target='_blank' rel="noreferrer"><BsLinkedin /></a>
+        <a href='https://github.com/Ysquid' target='_blank' rel="noreferrer"><BsGithub /></a>
+        <a href='https://twitter.com/YSquidcoding' target='_blank' rel="noreferrer"><FaTwitter/></a>
     </div>
 
   )
