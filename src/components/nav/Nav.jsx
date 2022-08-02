dab9ce1c1ee588e29c6714a9120784eff5ca700b
YSquid <102336493+YSquid@users.noreply.github.com>
2022-08-02 13:05:09 -0600

    Starting navbar
    Sketched the 5 links for navbar
    
    imported icons for each

--- src/components/nav/Nav.jsx
+++ src/components/nav/Nav.jsx
@@ -1,9 +1,20 @@
 import React from 'react'
 import './nav.css'
+import {AiOutlineUser, AiOutlineFundProjectionScreen, AiOutlineHome, AiOutlineContacts} from 'react-icons/ai'
+import {MdComputer} from 'react-icons/md'
+
 
 const Nav = () => {
   return (
-    <div>Nav</div>
+    <nav>
+      <a href='#'><AiOutlineHome /></a>
+      <a href='#about'><AiOutlineUser /></a>
+      <a href='#experience'><AiOutlineFundProjectionScreen /></a>
+      <a href='#services'><MdComputer /></a>
+      <a href='#contact'><AiOutlineContacts /></a>
+      
+
+    </nav>
   )
 }
 
