6f88eb39daa2ff6939bf2599151ddb355e363727
YSquid <ahk648@gmail.com>
2022-08-04 11:52:32 -0600

    About section
    About section icons and classes

--- src/components/about/About.jsx
+++ src/components/about/About.jsx
@@ -1,6 +1,9 @@
 import React from 'react'
 import './about.css'
 import ME from '../../assets/about-me-avatar.png'
+import {BsDiagram3Fill} from 'react-icons/bs'
+import {FaUserFriends} from 'react-icons/fa'
+import {RiFolderChartFill} from 'react-icons/ri'
 
 const About = () => {
   return (
@@ -18,8 +21,21 @@ const About = () => {
       <div className="about__content">
       <div className="about__cards">
         <article className="about__card">
+          <BsDiagram3Fill className='about__icon'/>
           <h5>Experience</h5>
           <small>1 year</small>
+        </article>
+
+        <article className="about__card">
+          <FaUserFriends className='about__icon' />
+          <h5>Clients</h5>
+          <small>Across Canada & U.S.</small>
+        </article>
+
+        <article className="about__card">
+          <RiFolderChartFill className='about__icon' />
+          <h5>Clients</h5>
+          <small>Across Canada & U.S.</small>
         </article>
 
 
