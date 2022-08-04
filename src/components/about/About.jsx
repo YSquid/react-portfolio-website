9132216a953a2f1a83adbf02292fdf994ee8c3a4
YSquid <ahk648@gmail.com>
2022-08-04 10:34:52 -0600

    Adding a little about
    Added the about image
    
    fixed medium screen media query to show 100vh for the header section

--- src/components/about/About.jsx
+++ src/components/about/About.jsx
@@ -1,5 +1,6 @@
 import React from 'react'
 import './about.css'
+import ME from '../../assets/about-me-avatar.png'
 
 const About = () => {
   return (
@@ -8,6 +9,23 @@ const About = () => {
     <h2>About me</h2>
 
     <div className="contaier about__container"></div>
+      <div className="about__me">
+        <div className="about__me__image">
+          <img src={ME}alt="About Me" />
+        </div>
+      </div>
+
+      <div className="about__content">
+      <div className="about__cards">
+        <article className="about__card">
+          <h5>Experience</h5>
+          <small>1 year</small>
+        </article>
+
+
+      </div>
+
+      </div>
     </section>
   )
 }
