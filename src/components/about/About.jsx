49d8ac0603e82699e0778baa99fee83c69efeef4
YSquid <ahk648@gmail.com>
2022-08-04 01:13:18 -0600

    About component structure
    Divs for about component
    
    headers
    
    need to get new image about-me.lpg

--- src/components/about/About.jsx
+++ src/components/about/About.jsx
@@ -3,7 +3,12 @@ import './about.css'
 
 const About = () => {
   return (
-    <section id="about">About</section>
+  <section id="about">
+    <h5>Get To Know</h5>
+    <h2>About me</h2>
+
+    <div className="contaier about__container"></div>
+    </section>
   )
 }
 
