9d82e2535e24e0be28f6de8287e24d83c7418573
YSquid <102336493+YSquid@users.noreply.github.com>
2022-08-15 09:41:35 -0600

    Setting up experience section

--- src/components/experience/Experience.jsx
+++ src/components/experience/Experience.jsx
@@ -1,9 +1,42 @@
 import React from 'react'
 import './experience.css'
+import {BsPatchCheckFill} from 'react-icons/bs'
 
 const Experience = () => {
   return (
-    <section id="experience">Experience</section>
+    <section id="experience">
+
+    <h2>My Skills</h2>
+
+    <div className="container experience__container">
+      <div className="experience__frontend">
+        <h3>Frontend Development</h3>
+        <div className="experience__content">
+          <article className="experience__details">
+            <BsPatchCheckFill />
+          </article>
+        </div>
+
+      </div>
+
+      <div className="experience__backend"></div>
+
+
+
+
+    </div>
+
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    </section>
+
   )
 }
 
