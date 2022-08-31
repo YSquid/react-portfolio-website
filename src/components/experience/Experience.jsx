9c9795d5d7a51fe5df5ea52a33692f858ef5c337
YSquid <102336493+YSquid@users.noreply.github.com>
2022-08-31 09:40:42 -0600

    Finishing Experience and styling about
    Finished Experience section
    
    Experience CSS + working media queries
    
    Trying to fix media query for small screen on about (its working for medium, but on small cards won't align)

--- src/components/experience/Experience.jsx
+++ src/components/experience/Experience.jsx
@@ -13,14 +13,96 @@ const Experience = () => {
         <h3>Frontend Development</h3>
         <div className="experience__content">
           <article className="experience__details">
-            <BsPatchCheckFill />
+            <BsPatchCheckFill className='experience__details-icon'/>
+            <div>
+              <h4>HTML</h4>
+              <small className='text-light'>Experienced</small>
+            </div>
+          </article>
+          <article className="experience__details">
+            <BsPatchCheckFill className='experience__details-icon'/>
+            <div>
+              <h4>CSS</h4>
+              <small className='text-light'>Experienced</small>
+            </div>
+          </article>
+          <article className="experience__details">
+            <BsPatchCheckFill className='experience__details-icon'/>
+            <div>
+              <h4>Javascript</h4>
+              <small className='text-light'>Experienced</small>
+            </div>
+          </article>
+          <article className="experience__details">
+            <BsPatchCheckFill className='experience__details-icon'/>
+            <div>
+              <h4>React JS</h4>
+              <small className='text-light'>Experienced</small>
+            </div>
+          </article>
+          <article className="experience__details">
+            <BsPatchCheckFill className='experience__details-icon'/>
+            <div>
+              <h4>Bootstrap</h4>
+              <small className='text-light'>Intermediate</small>
+            </div>
+          </article>
+          <article className="experience__details">
+            <BsPatchCheckFill className='experience__details-icon'/>
+            <div>
+              <h4>Tailwind CSS</h4>
+              <small className='text-light'>Intermediate</small>
+            </div>
           </article>
         </div>
 
       </div>
+      {/*END OF FRONTEND*/}
 
-      <div className="experience__backend"></div>
+      <div className="experience__backend">
+        <h3>Backend Development</h3>
+        <div className="experience__content">
+          <article className="experience__details">
+            <BsPatchCheckFill className='experience__details-icon'/>
+            <div>
+              <h4>Node JS</h4>
+              <small className='text-light'>Introductory</small>
+            </div>
+          </article>
+          <article className="experience__details">
+            <BsPatchCheckFill className='experience__details-icon'/>
+            <div>
+              <h4>MySQL</h4>
+              <small className='text-light'>Introductory</small>
+            </div>
+          </article>
+          <article className="experience__details">
+            <BsPatchCheckFill className='experience__details-icon'/>
+            <div>
+              <h4>Mongo DB</h4>
+              <small className='text-light'>Introductory</small>
+            </div>
+          </article>
+          <article className="experience__details">
+            <BsPatchCheckFill className='experience__details-icon'/>
+            <div>
+              <h4>Python</h4>
+              <small className='text-light'>Introductory</small>
+            </div>
+          </article>
+          <article className="experience__details">
+            <BsPatchCheckFill className='experience__details-icon'/>
+            <div>
+              <h4>PHP</h4>
+              <small className='text-light'>Introductory</small>
+            </div>
+          </article>
+        </div>
 
+
+
+
+      </div>
 
 
 
