aece650a6c9a4ed142edc3745366141038ad1298
YSquid <102336493+YSquid@users.noreply.github.com>
2022-09-01 09:30:24 -0600

    Services and portfolio done. Testimonials Started
    Finished the services part
    
    Finished portfolio. Used an array of portfolio data and mapped through
    
    Started testimonials

--- src/components/portfolio/Portfolio.jsx
+++ src/components/portfolio/Portfolio.jsx
@@ -1,10 +1,72 @@
-import React from 'react'
-import './portfolio.css'
+import React from "react";
+import "./portfolio.css";
+import IMG1 from "../../assets/portfolio1.jpg";
+import IMG2 from "../../assets/portfolio2.jpg";
+import IMG3 from "../../assets/portfolio3.jpg";
+import IMG4 from "../../assets/portfolio4.jpg";
+import IMG5 from "../../assets/portfolio5.png";
+import IMG6 from "../../assets/portfolio6.jpg";
+
+const data = [
+  {
+    id: 1,
+    image: IMG1,
+    title: 'Item1',
+    github: 'https://github.com/Ysquid',
+    demo: 'https://dribbble.com'
+
+  },
+  {
+    id: 2,
+    image: IMG2,
+    title: 'Item2',
+    github: 'https://github.com',
+    demo: 'https://dribbble.com'
+  }
+]
 
 const Portfolio = () => {
   return (
-    <section id="portfolio">Portfolio</section>
-  )
-}
+    <section id="portfolio">
+      <h5>Portfolio</h5>
 
-export default Portfolio
+      <div className="container portfolio__container">
+
+        {
+          
+          data.map(({id, image, title, github, demo}) =>{
+            return (
+              <article key={id} className="portfolio__item">
+              <div className="portfolio__item-img">
+              <img src={image} alt={title} />
+              </div>
+              <h3>{title}</h3>
+              <div className="portfolio__item-cta">
+              <a href={github} className="btn" target="_blank">
+              GitHub
+              </a>
+              <a
+              href={demo}
+              className="btn btn-primary"
+              target="_blank"
+              >
+              Live Demo
+              </a>
+            </div>
+            </article>
+            )
+
+          })
+
+
+
+        }
+       
+      </div>
+    </section>
+  );
+};
+
+export default Portfolio;
+
+
--- src/components/testimonials/Testimonials.jsx
+++ src/components/testimonials/Testimonials.jsx
@@ -1,9 +1,55 @@
 import React from 'react'
 import './testimonials.css'
+import AVATAR1 from '../../assets/avatar1.jpg'
+import AVATAR2 from '../../assets/avatar2.jpg'
+import AVATAR3 from '../../assets/avatar3.jpg'
+import AVATAR4 from '../../assets/avatar4.jpg'
 
 const Testimonials = () => {
   return (
-    <section id="testimonials">Testimonials</section>
+    <section id="testimonials">
+      <h5>Testimonials</h5>
+
+      <div className="container testimonials__container">
+        <article className="testimonial">
+          <div className="client__avatar">
+            <img src={AVATAR1} alt="Avatar One" />
+          </div>
+          <h5 className="client__name">Ernest Achiever</h5>
+            <small className="client__review">
+             Lorem ipsum dolor sit, amet consectetur adipisicing elit. Accusamus, consequuntur.
+            </small>
+          </article>
+
+          <article className="testimonial">
+          <div className="client__avatar">
+            <img src={AVATAR1} alt="Avatar One" />
+          </div>
+          <h5 className="client__name">Ernest Achiever</h5>
+            <small className="client__review">
+             Lorem ipsum dolor sit, amet consectetur adipisicing elit. Accusamus, consequuntur.
+            </small>
+          </article>
+
+          <article className="testimonial">
+          <div className="client__avatar">
+            <img src={AVATAR1} alt="Avatar One" />
+          </div>
+          <h5 className="client__name">Ernest Achiever</h5>
+            <small className="client__review">
+             Lorem ipsum dolor sit, amet consectetur adipisicing elit. Accusamus, consequuntur.
+            </small>
+          </article>
+
+     
+      </div>
+
+
+
+
+
+
+    </section>
   )
 }
 
