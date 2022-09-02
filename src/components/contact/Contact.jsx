db188746d8f9d2f05ef917a905f21f71cd5afc95
YSquid <102336493+YSquid@users.noreply.github.com>
2022-09-02 10:00:23 -0600

    Contact, footer, build
    Built the contact and footer pages
    
    Used emailJS for contact form
    
    Added homepage in our package json
    
    ran npm build

--- src/components/contact/Contact.jsx
+++ src/components/contact/Contact.jsx
@@ -1,9 +1,65 @@
 import React from 'react'
 import './contact.css'
+import {AiOutlineMail} from 'react-icons/ai'
+import {RiMessengerLine} from 'react-icons/ri'
+import {BsPhone} from 'react-icons/bs'
+import {useRef} from 'react';
+import emailjs from 'emailjs-com';
+
 
 const Contact = () => {
+
+  const form = useRef();
+  const sendEmail = (e) => {
+    e.preventDefault();
+
+    emailjs.sendForm('service_0yqopkd', 'template_odoxcdi', form.current, 'Iv-UOvg-qxM985Ac3')
+      
+    e.target.reset();
+  };
   return (
-    <section id="contact">Contact</section>
+    <section id="contact">
+      <h5>Get in Touch</h5>
+
+      <div className="container contact__container">
+        <div className="contact__options">
+          <article className="contact__option">
+            <AiOutlineMail className='contact-icon'/>
+            <h4>Email</h4>
+            <h5>ahmad.k@ysquid.tech</h5>
+            <a href="mailto:ahmad.k@ysquid.tech" target="_blank">Send an Email</a>
+          </article>
+
+          <article className="contact__option">
+            <RiMessengerLine className='contact-icon' />
+            <h4>Messenger</h4>
+            <h5>Ahmad Kariem</h5>
+            <a href="https://m.me/ahmad.a.kariem" target="_blank">Message Me</a>
+          </article>
+
+          <article className="contact__option">
+            <BsPhone className='contact-icon' />
+            <h4>Cell</h4>
+            <h5>(306)-491-6395</h5>
+            <a href="tel:3064916395" target="_blank">Call or Text</a>
+          </article>
+        </div>
+
+        <form ref={form} onSubmit={sendEmail}>
+          <input type="text" name='name' placeholder='Your Full Name' required />
+          <input type="email" name='email' placeholder='Your Email' required />
+          <textarea name="message" rows="7" placeholder='Your Message' required></textarea>
+          <button className="btn btn-primary">Send Message</button>
+        </form>
+          
+
+
+
+
+
+      </div>
+
+    </section>
   )
 }
 
--- src/components/footer/Footer.jsx
+++ src/components/footer/Footer.jsx
@@ -1,9 +1,35 @@
 import React from 'react'
 import './footer.css'
+import {BsLinkedin, BsGithub} from 'react-icons/bs'
+import {FaTwitter} from 'react-icons/fa'
 
 const Footer = () => {
   return (
-    <div>Footer</div>
+    <footer>
+      <a href="#" className='footer__logo'>Y Squid Tech</a>
+      
+      <ul className="permalinks">
+        <li><a href="#">Home</a></li>
+        <li><a href="#about">About</a></li>
+        <li><a href="#experience">Experience</a></li>
+        <li><a href="#services">Services</a></li>
+        <li><a href="#portfolio">Portfolio</a></li>
+        <li><a href="#testimonials">Testimonials</a></li>
+        <li><a href="#contact">Contact</a></li>
+      </ul>
+
+      <div className="footer__socials">
+        <a href='https://linkedin.com/in/ahmad-kariem/' target='_blank' rel="noreferrer"><BsLinkedin /></a>
+        <a href='https://github.com/Ysquid' target='_blank' rel="noreferrer"><BsGithub /></a>
+        <a href='https://twitter.com/YSquidcoding' target='_blank' rel="noreferrer"><FaTwitter/></a>
+      </div>
+
+      <div className="footer__copyright">
+        <small>&copy; Ahmad Kariem. All rights reserved.</small>
+      </div>
+
+
+    </footer>
   )
 }
 
