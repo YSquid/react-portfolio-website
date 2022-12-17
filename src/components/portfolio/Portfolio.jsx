b2926505718e723cdc3d7f629acd6e6b61b80f27
Ysquid <ahk648@gmail.com>
2022-12-17 17:18:00 -0600

    added tech. resized images. changed layouts

--- src/components/portfolio/Portfolio.jsx
+++ src/components/portfolio/Portfolio.jsx
@@ -7,38 +7,45 @@ const data = [
   {
     id: 1,
     image: redditClone,
-    title: "Item1",
+    title: "Reddit Clone",
     github: "https://github.com/Ysquid",
-    demo: "https://dribbble.com",
+    site: "https://dribbble.com",
+    tech: ["React", "Redux", "HTML5", "CSS3", "Git/GitHub"],
   },
   {
     id: 2,
     image: mrkCBD,
-    title: "Item2",
+    title: "CBD Brand Landing Page",
     github: "https://github.com",
-    demo: "https://dribbble.com",
+    site: "https://dribbble.com",
+    tech: ["HTML5", "CSS3", "Git/GitHub"],
   },
 ];
 
 const Portfolio = () => {
   return (
     <section id="portfolio">
-      <h5>Portfolio</h5>
+      <h2>Portfolio</h2>
 
       <div className="container portfolio__container">
-        {data.map(({ id, image, title, github, demo }) => {
+        {data.map(({ id, image, title, github, site, tech }) => {
           return (
             <article key={id} className="portfolio__item">
               <div className="portfolio__item-img">
                 <img src={image} alt={title} />
               </div>
               <h3>{title}</h3>
+              <ul className="tech">
+                {tech.map((elem) => {
+                  return <li className="tech__item">{elem}</li>;
+                })}
+              </ul>
               <div className="portfolio__item-cta">
                 <a href={github} className="btn" target="_blank">
                   GitHub
                 </a>
-                <a href={demo} className="btn btn-primary" target="_blank">
-                  Live Demo
+                <a href={site} className="btn btn-primary" target="_blank">
+                  View Site
                 </a>
               </div>
             </article>
