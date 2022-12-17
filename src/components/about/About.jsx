9c6b282172806b2d5a0279c5040bc53451ed7ed3
Ysquid <ahk648@gmail.com>
2022-12-17 11:53:28 -0600

    about section - good on all devices vertical

--- src/components/about/About.jsx
+++ src/components/about/About.jsx
@@ -1,53 +1,52 @@
-import React from 'react'
-import './about.css'
-import ME from '../../assets/me.jpg'
-import {BsDiagram3Fill} from 'react-icons/bs'
-import {FaUserFriends} from 'react-icons/fa'
-import {RiFolderChartFill} from 'react-icons/ri'
+import React from "react";
+import "./about.css";
+import ME from "../../assets/me.jpg";
+import { BsDiagram3Fill } from "react-icons/bs";
+import { FaUserFriends } from "react-icons/fa";
+import { RiFolderChartFill } from "react-icons/ri";
 
 const About = () => {
   return (
-  <section id="about">
-    <h5>Get To Know</h5>
-    <h2>About me</h2>
+    <section id="about">
+      <h2>About me</h2>
 
-    <div className="container about__container">
-      <div className="about__me">
-        <div className="about__me__image">
-          <img src={ME}alt="About Me" />
-        </div>
-      </div>
-
-      <div className="about__content">
-        <div className="about__cards">
-          <article className="about__card">
-            <BsDiagram3Fill className='about__icon'/>
-            <h5>Experience</h5>
-            <small>1 year</small>
-          </article>
-
-          <article className="about__card">
-            <FaUserFriends className='about__icon' />
-            <h5>Clients</h5>
-            <small>Across Canada & U.S.</small>
-          </article>
-
-          <article className="about__card">
-            <RiFolderChartFill className='about__icon' />
-            <h5>Projects</h5>
-            <small>10+ websites</small>
-          </article>
-
-        <p>I am a self taught developer with 1 year experience building custom websites.</p>
-
-        <a href="#contact" className='btn btn-primary'>Let's Talk</a>
-
+      <div className="container about__container">
+        <div className="about__me">
+          <div className="about__me__image">
+            <img src={ME} alt="About Me" />
+          </div>
         </div>
 
+        <div className="about__content">
+          <div className="about__bio">
+            <p>Hi, I'm Ahmad.</p> <br></br>
+            <p>
+              I am a 100% self taught full stack developer from Saskatoon
+              Sasktchewan. After several years of professional experience
+              working in areas such as tech sales, marketing, and data analysis,
+              I decided to pursue a field that has always interested me -
+              computer programming.{" "}
+            </p>
+            <br></br>
+            <p>
+              In March of 2022 I started learning programming in my spare time.
+              In September of 2022 I began dedicating myself full-time to
+              learning full-stack web development.
+            </p>
+            <br></br>
+            <p>
+              My ultimate goal is to become a full-time professional developer.
+              I am open to in-person roles across Canada as well as remote
+              positions.
+            </p>
+          </div>
+          <a href="#contact" className="btn btn-primary">
+            Let's Talk
+          </a>
+        </div>
       </div>
-    </div>
     </section>
-  )
-}
+  );
+};
 
-export default About
+export default About;
