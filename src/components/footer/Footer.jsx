30eac17137d086a86d15867e4eef96674f58e950
Ysquid <ahk648@gmail.com>
2022-12-18 16:06:30 -0600

    updated footer - links, title. tested all device vert and horiz

--- src/components/footer/Footer.jsx
+++ src/components/footer/Footer.jsx
@@ -6,13 +6,12 @@ import {FaTwitter} from 'react-icons/fa'
 const Footer = () => {
   return (
     <footer>
-      <a href="#" className='footer__logo'>Y Squid Tech</a>
+      <a href="#" className='footer__logo'>Ahmad Kariem</a>
       
       <ul className="permalinks">
         <li><a href="#">Home</a></li>
         <li><a href="#about">About</a></li>
-        <li><a href="#experience">Experience</a></li>
-        <li><a href="#services">Services</a></li>
+        <li><a href="#experience">Skills</a></li>
         <li><a href="#portfolio">Portfolio</a></li>
         <li><a href="#testimonials">Testimonials</a></li>
         <li><a href="#contact">Contact</a></li>
@@ -21,7 +20,6 @@ const Footer = () => {
       <div className="footer__socials">
         <a href='https://linkedin.com/in/ahmad-kariem/' target='_blank' rel="noreferrer"><BsLinkedin /></a>
         <a href='https://github.com/Ysquid' target='_blank' rel="noreferrer"><BsGithub /></a>
-        <a href='https://twitter.com/YSquidcoding' target='_blank' rel="noreferrer"><FaTwitter/></a>
       </div>
 
       <div className="footer__copyright">
