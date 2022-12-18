2530b8002a5a2b6436fddf8593e82b14486d1df4
Ysquid <ahk648@gmail.com>
2022-12-18 16:39:30 -0600

    hover text for nav buttons. updated skills icon

--- src/components/nav/Nav.jsx
+++ src/components/nav/Nav.jsx
@@ -1,25 +1,72 @@
-import React from 'react'
-import './nav.css'
-import {AiOutlineUser, AiOutlineFundProjectionScreen, AiOutlineHome, AiOutlineContacts} from 'react-icons/ai'
-import {MdComputer} from 'react-icons/md'
-import {useState} from 'react'
-
+import React from "react";
+import "./nav.css";
+import {
+  AiOutlineUser,
+  AiOutlineFundProjectionScreen,
+  AiOutlineHome,
+  AiOutlineContacts,
+} from "react-icons/ai";
+import { MdComputer } from "react-icons/md";
+import {VscChecklist} from 'react-icons/vsc'
+import { useState } from "react";
 
 const Nav = () => {
-
-  const [activeNav, setActiveNav] = useState('#')
-
-
+  const [activeNav, setActiveNav] = useState("#");
 
   return (
     <nav>
-      <a href='#header' onClick={() => {setActiveNav('#header')}} className={activeNav === '#header' ? 'active' : ''}><AiOutlineHome /></a>
-      <a href='#about' onClick={() => {setActiveNav('#about')}} className={activeNav === '#about' ? 'active' : ''}><AiOutlineUser /></a>
-      <a href='#experience' onClick={() => {setActiveNav('#experience')}} className={activeNav === '#experience' ? 'active' : ''}><AiOutlineFundProjectionScreen /></a>
-      <a href='#portfolio' onClick={() => {setActiveNav('#services')}} className={activeNav === '#services' ? 'active' : ''}><MdComputer /></a>
-      <a href='#contact' onClick={() => {setActiveNav('#contact')}} className={activeNav === '#contact' ? 'active' : ''}><AiOutlineContacts /></a>
+      <a
+        href="#header"
+        onClick={() => {
+          setActiveNav("#header");
+        }}
+        className={activeNav === "#header" ? "active" : ""}
+        title="Home"
+      >
+        <AiOutlineHome />
+      </a>
+      <a
+        href="#about"
+        onClick={() => {
+          setActiveNav("#about");
+        }}
+        className={activeNav === "#about" ? "active" : ""}
+        title="About"
+      >
+        <AiOutlineUser />
+      </a>
+      <a
+        href="#experience"
+        onClick={() => {
+          setActiveNav("#experience");
+        }}
+        className={activeNav === "#experience" ? "active" : ""}
+        title="Skills"
+      >
+        <VscChecklist />
+      </a>
+      <a
+        href="#portfolio"
+        onClick={() => {
+          setActiveNav("#services");
+        }}
+        className={activeNav === "#services" ? "active" : ""}
+        title="Portfolio"
+      >
+        <MdComputer />
+      </a>
+      <a
+        href="#contact"
+        onClick={() => {
+          setActiveNav("#contact");
+        }}
+        className={activeNav === "#contact" ? "active" : ""}
+        title="Contact"
+      >
+        <AiOutlineContacts />
+      </a>
     </nav>
-  )
-}
+  );
+};
 
-export default Nav
+export default Nav;
