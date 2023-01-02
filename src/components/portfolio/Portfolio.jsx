cd0afa6b9e42acfba12701e26af891a54a3bb3ce
Ysquid <ahk648@gmail.com>
2023-01-02 09:29:41 -0600

    added project descriptions

--- src/components/portfolio/Portfolio.jsx
+++ src/components/portfolio/Portfolio.jsx
@@ -10,6 +10,7 @@ const data = [
     title: "Reddit Clone",
     github: "https://github.com/YSquid/reddit-minimal",
     site: "https://ak-reddit-minimal.netlify.app/",
+    description: "A basic functionality Reddit clone built with React and Redux. Utilizes the Reddit JSON API to fetch data from Reddit.",
     tech: ["React", "Redux", "HTML5", "CSS3", "Git/GitHub"],
   },
   {
@@ -18,6 +19,7 @@ const data = [
     title: "CBD Brand Landing Page",
     github: "https://github.com/YSquid/mrk-landing",
     site: "https://mrkcbd.com/",
+    description: "A one page site built with HTML and CSS for a CBD brand.",
     tech: ["HTML5", "CSS3", "Git/GitHub"],
   }
 ];
@@ -28,7 +30,7 @@ const Portfolio = () => {
       <h2>Portfolio</h2>
 
       <div className="container portfolio__container">
-        {data.map(({ id, image, title, github, site, tech }) => {
+        {data.map(({ id, image, title, github, site, description, tech }) => {
           return (
             <article key={id} className="portfolio__item">
               <div className="portfolio__item-img">
@@ -37,6 +39,7 @@ const Portfolio = () => {
                 </a>
               </div>
               <h3>{title}</h3>
+              <p className="project__description">{description}</p>
               <ul className="tech">
                 {tech.map((elem) => {
                   return <li className="tech__item">{elem}</li>;
