3507e9a3e7235bf376f789c67d379147a003c91f
Ysquid <ahk648@gmail.com>
2023-01-02 16:24:57 -0600

    google analytics pageviews added

--- src/App.jsx
+++ src/App.jsx
@@ -1,29 +1,37 @@
-import React from 'react'
-import './app.css'
-import Header from './components/header/Header'
-import Nav from './components/nav/Nav'
-import About from './components/about/About'
-import Experience from './components/experience/Experience'
-import Portfolio from './components/portfolio/Portfolio'
-import Contact from './components/contact/Contact'
-import Footer from './components/footer/Footer'
-import ReactGA from 'react-ga';
-ReactGA.initialize('G-DTNW0PWFKR');
-ReactGA.pageview(window.location.pathname + window.location.search);
-
+import React, { useEffect } from "react";
+import "./app.css";
+import Header from "./components/header/Header";
+import Nav from "./components/nav/Nav";
+import About from "./components/about/About";
+import Experience from "./components/experience/Experience";
+import Portfolio from "./components/portfolio/Portfolio";
+import Contact from "./components/contact/Contact";
+import Footer from "./components/footer/Footer";
+import { initGA, GAPageView } from "./index";
 
 const App = () => {
-  return (
-  <div className='app'>
-    <Header />
-    <Nav />
-    <About />
-    <Experience />
-    <Portfolio />
-    <Contact />
-    <Footer />
-  </div>
-  )
-}
+  //calling initGA on page load
+  useEffect(() => {
+    initGA()
+    console.log('initGA effect');
+  }, []);
 
-export default App
+  //calling GA pageview on load
+  useEffect(() => {
+    GAPageView("landing")
+    console.log('GAPageView effect');
+  }, []);
+  return (
+    <div className="app">
+      <Header />
+      <Nav />
+      <About />
+      <Experience />
+      <Portfolio />
+      <Contact />
+      <Footer />
+    </div>
+  );
+};
+
+export default App;
--- src/index.js
+++ src/index.js
@@ -1,6 +1,18 @@
 import ReactDOM from 'react-dom'
 import App from './App'
 import './index.css'
+import ReactGA from 'react-ga';
+
+//Google Analytics initialization function
+export const initGA = () => {       
+    ReactGA.initialize('G-DTNW0PWFKR');
+} 
+
+//Google Analytics pageview function
+export const GAPageView = (page) => {   
+    ReactGA.pageview(page);   
+}
+
 
 
 ReactDOM.render(<App/ >, document.querySelector('#root'))
