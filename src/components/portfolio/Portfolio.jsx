5ffd1e9a91467da6648f78d99f88f8b6b420e4d3
Ysquid <ahk648@gmail.com>
2023-07-29 16:18:24 -0600

    add gdp project. refactor data projects to include github

--- src/components/portfolio/Portfolio.jsx
+++ src/components/portfolio/Portfolio.jsx
@@ -2,56 +2,78 @@ import React from "react";
 import "./portfolio.css";
 import redditThumbnail from "../../assets/redditThumbnail.png";
 import mrkCBD from "../../assets/mrkCBD.png";
-import quoteMachine from '../../assets/quoteMachine.png'
-import appointmentPlanner from '../../assets/appointmentPlanner.png'
-import ahmadsEats from '../../assets/ahmadsEats.png'
-import FinancialReport from '../../assets/Financial-Report.zip'
-import salesData from '../../assets/sales-data.png'
+import quoteMachine from "../../assets/quoteMachine.png";
+import appointmentPlanner from "../../assets/appointmentPlanner.png";
+import ahmadsEats from "../../assets/ahmadsEats.png";
+import FinancialReport from "../../assets/Financial-Report.zip";
+import salesData from "../../assets/sales-data.png";
+import gdpLife from "../../assets/gdp_life.png"
 
 const dataProjects = [
   {
-    id: 'dp-ex1',
+    id: "dp-ex1",
     image: salesData,
-    type: 'powerBI',
+    type: "powerBI",
     title: "Sales Data Analysis",
-    preview: "https://github.com/YSquid/ecomm-client",
     report: FinancialReport,
-    description: "Analysis of sales data using Power BI and presented in PowerPoint",
-    tech: ["PowerBI",  "PowerPoint", "Excel"],
+    description:
+      "Analysis of sales data using Power BI and presented in PowerPoint",
+    tech: ["Python", "PowerPoint", "Excel"],
   },
-]
+  {
+    id: "dp-gdp",
+    image: gdpLife,
+    github: "https://github.com/YSquid/gdp_life",
+    title: "GDP & Life Expectancy",
+    preview: "https://github.com/YSquid/ecomm-client",
+    description:
+      "Analysis of relationship between GDP and life expectancy using world bank data",
+    tech: ["Python", "Matplotlib"],
+  },
+];
 
 const webProjects = [
   {
-    id: 'wd-eats',
+    id: "wd-eats",
     image: ahmadsEats,
     title: "E-commerce Store",
     github: "https://github.com/YSquid/ecomm-client",
     site: "https://ahmads-eats.netlify.app/",
-    description: "A mock e-commerce store. Features - user registration/authentication, placing orders, and viewing order history.",
-    tech: ["React", "Node.js", "Express.js", "PostgreSQL", "HTML5", "CSS3", "Git/GitHub"],
+    description:
+      "A mock e-commerce store. Features - user registration/authentication, placing orders, and viewing order history.",
+    tech: [
+      "React",
+      "Node.js",
+      "Express.js",
+      "PostgreSQL",
+      "HTML5",
+      "CSS3",
+      "Git/GitHub",
+    ],
   },
 
   {
-    id: 'wd-reddit',
+    id: "wd-reddit",
     image: redditThumbnail,
     title: "Reddit Clone",
     github: "https://github.com/YSquid/reddit-minimal",
     site: "https://ak-reddit-minimal.netlify.app/",
-    description: "A basic functionality Reddit clone built with React and Redux. Utilizes the Reddit JSON API to fetch data from Reddit.",
+    description:
+      "A basic functionality Reddit clone built with React and Redux. Utilizes the Reddit JSON API to fetch data from Reddit.",
     tech: ["React", "Redux", "HTML5", "CSS3", "Git/GitHub", "Google Analytics"],
   },
   {
-    id: 'wd-quote',
+    id: "wd-quote",
     image: quoteMachine,
     title: "Random Quote Machine",
     github: "https://github.com/YSquid/react-quote-machine",
     site: "https://ak-react-quotes.netlify.app/",
-    description: "A web app that fetches random quotes with the option to tweet the quote in one click.",
+    description:
+      "A web app that fetches random quotes with the option to tweet the quote in one click.",
     tech: ["React", "Bootstrap", "HTML5", "CSS3", "Git/GitHub"],
   },
   {
-    id: 'wd-appt',
+    id: "wd-appt",
     image: appointmentPlanner,
     title: "React Appointment Planner",
     github: "https://github.com/YSquid/react-appointment-planner-challenge",
@@ -60,7 +82,7 @@ const webProjects = [
     tech: ["React", "HTML5", "CSS3", "Git/GitHub"],
   },
   {
-    id: 'wd-cbd',
+    id: "wd-cbd",
     image: mrkCBD,
     title: "CBD Brand Landing Page",
     github: "https://github.com/YSquid/mrk-landing",
@@ -77,74 +99,96 @@ const Portfolio = () => {
 
       <h3>Data Projects</h3>
       <div className="container portfolio__container data__projects">
-        {dataProjects.map(({ id, type, image, title, preview ,report, description, tech }) => {
-          return (
-            <article key={id} className="portfolio__item">
-              <div className="portfolio__item-img">
-               
-                  <img src={image} alt={title} />
+        {dataProjects.map(
+          ({ id, type, github, image, title, preview, report, description, tech }) => {
+            return (
+              <article key={id} className="portfolio__item">
                 
-              </div>
-              <h3>{title}</h3>
-              <p className="project__description">{description} {type === "powerBI" ? <p>Note: PowerBI license required to view Power BI report</p> : null}</p>
-              <ul className="tech">
-                {tech.map((elem) => {
-                  return <li className="tech__item">{elem}</li>;
-                })}
-              </ul>
-              <div className="portfolio__item-cta">
-                <a
-                  href={report}
-                  className="btn btn-primary"
-                  target="_blank"
-                  rel="noreferrer"
-                >
-                  Download Files
-                </a>
-              </div>
-            </article>
-          );
-        })}
+                <div className="portfolio__item-img">
+                  <img src={image} alt={title} />
+                </div>
+                
+                <h3>{title}</h3>
+                <p className="project__description">
+                  {description}{" "}
+                  {type === "powerBI" ? (
+                    <p>
+                      Note: PowerBI license required to view Power BI report
+                    </p>
+                  ) : null}
+                </p>
+                <ul className="tech">
+                  {tech.map((elem) => {
+                    return <li className="tech__item">{elem}</li>;
+                  })}
+                </ul>
+                <div className="portfolio__item-cta">
+                  {type === "powerBI" ? (
+                    <a
+                      href={report}
+                      className="btn btn-primary"
+                      target="_blank"
+                      rel="noreferrer"
+                    >
+                      Download Files
+                    </a>
+                  ) : (
+                    <a
+                      href={github}
+                      className="btn btn-primary"
+                      target="_blank"
+                      rel="noreferrer"
+                    >
+                      GitHub
+                    </a>
+                  )}
+                </div>
+              </article>
+            );
+          }
+        )}
       </div>
-      
+
       <h3>Web Development Projects</h3>
       <div className="container portfolio__container web__projects">
-        {webProjects.map(({ id, image, title, github, site, description, tech }) => {
-          return (
-            <article key={id} className="portfolio__item">
-              <div className="portfolio__item-img">
-                <a href={site} target="_blank" rel="noreferrer">
-                  <img src={image} alt={title} />
-                </a>
-              </div>
-              <h3>{title}</h3>
-              <p className="project__description">{description}</p>
-              <ul className="tech">
-                {tech.map((elem) => {
-                  return <li className="tech__item">{elem}</li>;
-                })}
-              </ul>
-              <div className="portfolio__item-cta">
-                <a
-                  href={github}
-                  className="btn"
-                  target="_blank"
-                  rel="noreferrer"
-                >
-                  GitHub
-                </a>
-                <a
-                  href={site}
-                  className="btn btn-primary"
-                  target="_blank"
-                  rel="noreferrer"
-                >
-                  View Site
-                </a>
-              </div>
-            </article>
-          );
-        })}
+        {webProjects.map(
+          ({ id, image, title, github, site, description, tech }) => {
+            return (
+              <article key={id} className="portfolio__item">
+                <div className="portfolio__item-img">
+                  <a href={site} target="_blank" rel="noreferrer">
+                    <img src={image} alt={title} />
+                  </a>
+                </div>
+                <h3>{title}</h3>
+                <p className="project__description">{description}</p>
+                <ul className="tech">
+                  {tech.map((elem) => {
+                    return <li className="tech__item">{elem}</li>;
+                  })}
+                </ul>
+                <div className="portfolio__item-cta">
+                  <a
+                    href={github}
+                    className="btn"
+                    target="_blank"
+                    rel="noreferrer"
+                  >
+                    GitHub
+                  </a>
+                  <a
+                    href={site}
+                    className="btn btn-primary"
+                    target="_blank"
+                    rel="noreferrer"
+                  >
+                    View Site
+                  </a>
+                </div>
+              </article>
+            );
+          }
+        )}
       </div>
     </section>
   );
